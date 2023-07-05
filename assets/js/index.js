c0ebdce4cc89c322284f28ae19a63a9cb5e10055
JaySun0 <jaysun0@yandex.ru>
2023-07-05 12:58:40 +0300

    feat: add project files to continue coding

---
+++ assets/js/index.js
@@ -0,0 +1,192 @@
+import languages from './translate.js';
+
+const current = {
+  lang: 'en',
+  theme: 'light',
+}
+
+const domElements = {
+  sectionTitleWrappers: document.querySelectorAll('.section__title-wrapper'),
+  hero: document.querySelector('.hero'),
+  header: document.querySelector('.header'),
+  logo: document.querySelector('.header__logo'),
+  themeIcon: document.querySelector('.header__theme-icon'),
+  themeBtn: document.querySelector('.header__theme-btn'),
+  burgerMenu: document.querySelector('.header__burger-menu'),
+  navigation: document.querySelector('.nav'),
+  burgerSticks: document.querySelectorAll('.header__burger-stick'),
+  languageOptions: document.querySelectorAll('.lang-switch__option'),
+  seasonBtnsContainer: document.querySelector('.portfolio__buttons-wrapper'), 
+  portfolioImages: document.querySelectorAll('.portfolio__image'),
+  contacts: document.querySelector('.contacts'),
+  inputs: document.querySelectorAll('.contacts__input'),
+}
+
+
+// THEME MENU
+const themeNodes = {
+  color: {
+    body: ['.body', true],
+    sectionTitles: ['.section__title', false],
+    buttons: ['.btn', false],
+    logo: ['.header__logo', true],
+    themeBtn: ['.header__theme-icon', true],
+    langOption: ['.lang-switch__option', false],
+    burgerSticks: ['.header__burger-stick', false],
+    navOpen: ['.nav', true],
+    contactsTitle: ['.contacts__title', true],
+    contactsInputs: ['.contacts__input', false],
+    footerIcons: ['.footer__icon', false],
+  },
+  background: {
+    'header': [`url('assets/img/header-bg-dark.jpg')`, true],
+    'hero': [`url('assets/img/header-bg-dark.jpg')`, true],
+    'contacts': [`url('assets/img/contacts-bg-dark.jpg')`, true],
+    'section__title-wrapper': [`url('assets/svg/section-title-line-dark.svg')`, false],
+  },
+  image: {
+    'header__theme-icon': `assets/svg/dark.svg`,
+  }
+}
+
+function changeColor(element, notMultiple) {
+
+  switch (current.theme) {
+    case 'dark': 
+      if (notMultiple) document.querySelector(element).classList.remove('light');
+      else document.querySelectorAll(element).forEach(el => el.classList.remove('light'));
+      break;
+
+    case 'light': 
+      if (notMultiple) document.querySelector(element).classList.add('light');
+      else document.querySelectorAll(element).forEach(el => el.classList.add('light'));
+      break;
+
+    default: console.log(`Theme ${current.theme}`);
+  }
+}
+
+function changeBackground(node, url, theme) {
+  const notMultiple = url[1];
+  const relevantUrl = url[0].replace(/[a-z]*(?=\.[a-z]{3})/, theme); 
+  if (notMultiple) document.querySelector(`.${node}`).style.backgroundImage = relevantUrl;
+  else document.querySelectorAll(`.${node}`).forEach(el => el.style.backgroundImage = relevantUrl);
+}
+
+function changeImage(element, attribute, theme) {
+  const relevantAttribute = attribute.replace(/[a-z]*(?=\.[a-z]{3})/, theme);
+  element.setAttribute('src', relevantAttribute)
+}
+
+
+function changeTheme(theme) {
+  current.theme = theme
+  domElements.themeBtn.setAttribute('data-theme', theme);
+  Object.keys(themeNodes.color).forEach(node => {
+    changeColor(themeNodes.color[node][0], themeNodes.color[node][1])
+  });
+  Object.keys(themeNodes.background).forEach(node => {
+    changeBackground(node, themeNodes.background[node], theme);
+  });
+  Object.keys(themeNodes.image).forEach(node => {
+    changeImage(document.querySelector(`.${node}`), themeNodes.image[node], theme);
+  });
+}
+
+domElements.themeBtn.addEventListener('click', (event) => {
+  const theme = event.currentTarget.dataset.theme === 'dark' ? 'light' : 'dark';
+  changeTheme(theme);
+})
+
+
+// BURGER MENU
+domElements.burgerMenu.addEventListener('click', () => {
+  if (window.innerWidth < 816) {
+    domElements.navigation.classList.toggle('open');
+    domElements.burgerSticks.forEach(stick => stick.classList.toggle('open'));
+  }
+})
+
+domElements.navigation.addEventListener('click', (e) => {
+  if(e.target.classList.contains('nav__item') && window.innerWidth < 816) {
+    domElements.navigation.classList.toggle('open');
+    domElements.burgerSticks.forEach(stick => stick.classList.toggle('open'));
+  }
+})
+
+
+// LANGUAGE MENU
+function translateNode (node, text) {
+  if (typeof text !== 'object') {
+     document.querySelector(`.${node}`).textContent = text
+  } else {
+    if (node === 'contacts__input') {
+      const inputs = document.querySelectorAll(`.contacts__input`);
+      inputs[0].setAttribute('placeholder', text[0]);
+      inputs[1].setAttribute('placeholder', text[1]);
+      inputs[2].setAttribute('placeholder', text[2]);
+    } else {
+      const nodes = document.querySelectorAll(`.${node}`);
+      nodes.forEach((node, ind) => {
+        const number = text.length > 1 ? ind : 0;
+        node.textContent = text[number];
+      })
+    }
+  }
+}
+
+function changeLanguage(lang) {
+  current.lang = lang;
+  if (lang === domElements.languageOptions[0].textContent) {
+    domElements.languageOptions[0].classList.add('lang-switch__option_active');
+    domElements.languageOptions[1].classList.remove('lang-switch__option_active');
+  } else {
+    domElements.languageOptions[1].classList.add('lang-switch__option_active');
+    domElements.languageOptions[0].classList.remove('lang-switch__option_active');
+  }
+  Object.keys(languages[lang]).forEach(key => {
+    translateNode(key, languages[lang][key], lang);
+  });
+}
+
+domElements.languageOptions.forEach((option, ind) => {
+  option.addEventListener('click', () => {
+    const lang = option.textContent;
+    changeLanguage(lang);
+  })
+})
+
+
+// SEASONS MENU
+domElements.seasonBtnsContainer.addEventListener('click', function (event) {
+  const targetBtn = event.target;
+  if (targetBtn.classList.contains('btn')) {
+    const season = targetBtn.classList[3].split('_')[3];
+    //change active btn
+    Array.from(this.children).forEach(btn => btn.classList.remove('portfolio__btn_active'));
+    targetBtn.classList.contains('btn') && targetBtn.classList.add('portfolio__btn_active');
+    //change images source
+    domElements.portfolioImages.forEach((img, ind) => img.setAttribute('src', `assets/img/${season}/${ind + 1}.jpg`));
+  } 
+})
+
+
+// PAGE RELOAD
+window.onunload = function() {
+  localStorage.setItem('theme', current.theme);
+  localStorage.setItem('lang', current.lang);
+}
+
+window.onload = function() {
+  const userTheme = localStorage.getItem('theme');
+  const userLang = localStorage.getItem('lang');
+
+  userLang ? changeLanguage(userLang) : changeLanguage('en');
+  userTheme && changeTheme(userTheme);
+}
+
+domElements.logo.addEventListener('click', () => {
+  document.location.reload();
+});
+
+console.log('Всё в порядке 82-85 :)');
---
+++ assets/js/translate.js
@@ -0,0 +1,140 @@
+const languages = {
+  'en': {
+    //navigation items
+    'nav__item_skills': 'skills',
+    'nav__item_portfolio': 'portfolio',
+    'nav__item_video': 'video',
+    'nav__item_price': 'price',
+    'nav__item_contacts': 'contacts',
+
+    //section names
+    'skills__title': 'Skills',
+    'portfolio__title': 'Portfolio',
+    'video__title': 'Video',
+    'price__title': 'Price',
+
+    //hero section
+    'hero__title': 'Alexa Rise',
+    'hero__motto': 'Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise',
+    'hero__button': 'Hire me',
+
+    //skills section
+    'skills__name_1': 'Digital photography',
+    'skills__description_1': 'High-quality photos in the studio and on the nature',
+    'skills__name_2': 'Video shooting',
+    'skills__description_2': 'Capture your moments so that they always stay with you',
+    'skills__name_3': 'Rotouch',
+    'skills__description_3': 'I strive to make photography surpass reality',
+    'skills__name_4': 'Audio',
+    'skills__description_4': 'Professional sounds recording for video, advertising, portfolio',
+
+     //portfolio section
+    'portfolio__btn_winter': 'Winter',
+    'portfolio__btn_spring': 'Spring',
+    'portfolio__btn_summer': 'Summer',
+    'portfolio__btn_autumn': 'Autumn',
+
+    //price section
+    'price__title_standard': 'Standard',
+    'price__price_standard': '500 $',
+    'price__title_premium': 'Premium',
+    'price__price_premium': '700 $',
+    'price__title_gold': 'Gold',
+    'price__price_gold': '1000 $',
+    'price__option_standard-1': 'One location',
+    'price__option_standard-2': '120 photos in color',
+    'price__option_standard-3': '12 photos in retouch',
+    'price__option_standard-4': 'Readiness 2-3 weeks',
+    'price__option_standard-5': 'Make up, visage',
+    'price__option_premium-1': 'One or two locations',
+    'price__option_premium-2': '200 photos in color',
+    'price__option_premium-3': '20 photos in retouch',
+    'price__option_premium-4': 'Readiness 1-2 weeks',
+    'price__option_premium-5': 'Make up, visage',
+    'price__option_gold-1': 'Three locations or more',
+    'price__option_gold-2': '300 photos in color',
+    'price__option_gold-3': '50 photos in retouch',
+    'price__option_gold-4': 'Readiness 1 week',
+    'price__option_gold-5': 'Make up, visage, hairstyle',
+    'price__button': ['Order shooting'],
+
+    //contacts section
+    'contacts__title': 'Contact me',
+    'contacts__button': 'Send message',
+    'contacts__input': ['E-mail', 'Phone', 'Message'],
+
+    //footer 
+    'footer__link_school': 'Rolling Scopes School',
+    'footer__link_github': 'github'
+  },
+
+  'ru': {
+    //navigation items
+    'nav__item_skills': 'навыки',
+    'nav__item_portfolio': 'портфолио',
+    'nav__item_video': 'видео',
+    'nav__item_price': 'цены',
+    'nav__item_contacts': 'контакты',
+
+    //section names
+    'skills__title': 'Навыки',
+    'portfolio__title': 'Портфолио',
+    'video__title': 'Видео',
+    'price__title': 'Цены',
+
+    //hero section
+    'hero__title': 'Алекса Райс',
+    'hero__motto': 'Сохраните искренние эмоции, романтические переживания и счастливые моменты жизни вместе с профессиональным фотографом',
+    'hero__button': 'Пригласить',
+
+    //skills section
+    'skills__name_1': 'Фотография',
+    'skills__description_1': 'Высококачественные фото в студии и на природе',
+    'skills__name_2': 'Видеосъемка',
+    'skills__description_2': 'Запечатлите лучшие моменты, чтобы они всегда оставались с вами',
+    'skills__name_3': 'Ретушь',
+    'skills__description_3': 'Я стремлюсь к тому, чтобы фотография превосходила реальность',
+    'skills__name_4': 'Звук',
+    'skills__description_4': 'Профессиональная запись звука для видео, рекламы, портфолио',
+
+     //portfolio section
+    'portfolio__btn_winter': 'Зима',
+    'portfolio__btn_spring': 'Весна',
+    'portfolio__btn_summer': 'Лето',
+    'portfolio__btn_autumn': 'Осень',
+
+    //price section
+    'price__title_standard': 'Стандарт',
+    'price__price_standard': '33000 ₱',
+    'price__title_premium': 'Премиум',
+    'price__price_premium': '46000 ₱',
+    'price__title_gold': 'Золотой',
+    'price__price_gold': '65000 ₱',
+    'price__option_standard-1': 'Одна локация',
+    'price__option_standard-2': '120 цветных фото',
+    'price__option_standard-3': '12 отретушированных фото',
+    'price__option_standard-4': 'Готовность через 2-3 недели',
+    'price__option_standard-5': 'Макияж, визаж',
+    'price__option_premium-1': 'Одна-две локации',
+    'price__option_premium-2': '200 цветных фото',
+    'price__option_premium-3': '20 отретушированных фото',
+    'price__option_premium-4': 'Готовность через 1-2 недели',
+    'price__option_premium-5': 'Макияж, визаж',
+    'price__option_gold-1': 'Три локации и больше',
+    'price__option_gold-2': '300 цветных фото',
+    'price__option_gold-3': '50 отретушированных фото',
+    'price__option_gold-4': 'Готовность через 1 неделю',
+    'price__option_gold-5': 'Макияж, визаж, прическа',
+    'price__button': ['Заказать съёмку'],
+    
+    //contacts section
+    'contacts__title': 'Свяжитесь со мной',
+    'contacts__button': 'Отправить',
+    'contacts__input': ['Электронная почта', 'Телефон', 'Cooбщение'],
+
+    //footer 
+    'footer__link_school': 'Школа Rolling Scopes',
+    'footer__link_github': 'гитхаб'
+  }
+}
+export default languages;
